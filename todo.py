91c72e9e6c05be8b0c34906324030a091e3f630b
Aleksandra Daņilova <144416304+davanilla@users.noreply.github.com>
2025-05-20 20:29:15 +0000

    Uzdevumu glabāšana

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import heapq
+import json
 
 class Task:
     def __init__(self, name, deadline_str, priority, category="General"):
@@ -36,12 +37,42 @@ def get_user_input():
         deadline_str = input("Deadline (YYYY.MM.DD HH:MM): ")
         priority = int(input("Priority (1 to 100): "))
         category = input("Category (default = 'General'): ")
+        if not category:
+            category = "General"
         task = Task(name, deadline_str, priority, category)
         tasks.append(task)
         print(f"Added: {task}\n") 
     return tasks
 
+def save_tasks(tasks, filename="todolist.json"):
+    tasks_data = []
+    for t in tasks:
+        tasks_data.append({
+            "name": t.name,
+            "deadline": t.deadline.strftime("%Y.%m.%d %H:%M"),
+            "priority": t.priority,
+            "category": t.category
+        })
+    with open(filename, "w") as f:
+        json.dump(tasks_data, f, indent=2)
+    print(f"Tasks saved to {filename}")
+
+def load_tasks(filename="todolist.json"):
+    tasks =[]
+    try:
+        with open(filename, "r") as f:
+            tasks_data = json.load(f)
+            for t in tasks_data:
+                tasks.append(Task(t["name"], t["deadline"], t["priority"], t["category"]))
+        print(f"Loaded {len(tasks)} tasks from {filename}")
+    except FileNotFoundError:
+        print("To-do list is void.")
+    return tasks
+
 if __name__ == "__main__":
-    user_tasks = get_user_input()
-    schedule_tasks(user_tasks)
+    tasks = load_tasks()
+    new_tasks = get_user_input()
+    tasks.extend(new_tasks)
+    save_tasks(tasks)
+    schedule_tasks(tasks)
     
